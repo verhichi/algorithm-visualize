e1d8a4c5275e2bed681d3f2972ebd35a65acff51
Daichi Nishida <verhichi@hotmail.com>
2020-05-06 02:08:18 +0900

    feat: atoms/Text and molecules/Button and add styles

--- src/components/atoms/Text/Text.tsx
+++ src/components/atoms/Text/Text.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import clsx from 'clsx'
 import Typography, { TypographyProps } from '@material-ui/core/Typography'
 
-const Text = ({ children, ...props }: TypographyProps) => <Typography {...props}>{children}</Typography>
+type Props = {
+  className?: string
+} & TypographyProps
+
+const Text = ({ className, children, ...props }: Props) => (
+  <Typography classes={{ root: clsx(className) }} {...props}>
+    {children}
+  </Typography>
+)
 
 export default Text
--- src/pages/Sort/BubbleSort/BubbleSort.tsx
+++ src/pages/Sort/BubbleSort/BubbleSort.tsx
@@ -2,36 +2,45 @@ import React, { useContext, useState, useRef } from 'react'
 import { Stage, Layer, Rect, Group } from 'react-konva'
 import Konva from 'konva'
 import Text from 'components/atoms/Text'
-import { initNumberArray, makeSortItemArray } from '.'
+import Button from 'components/molecules/Button'
 import { Rect as RectType } from 'konva/types/shapes/Rect'
 import { Group as GroupType } from 'konva/types/Group'
+import { initNumberArray, makeSortItemArray } from '.'
 import { BodyWidthContext } from 'layouts/Body'
 import './styles.css'
 
 const BubbleSort = () => {
-  const width = useContext(BodyWidthContext)
+  const canvasContainer = useRef<HTMLDivElement>(null)
+  const canvasContainerHeight = 500
+  const bodyWidth = useContext(BodyWidthContext)
   const [isSorting, setIsSorting] = useState(false)
   const sortItemRefs = useRef<{ [key: number]: RectType | null }>({})
   const selectedItemRef = useRef<GroupType | null>()
   const sortItems = makeSortItemArray(initNumberArray)
+  const sortItemsMax = Math.max(...initNumberArray)
+  const barWidth = bodyWidth / (sortItems.length * 2)
+  const selectedBarIndicatorWidth = barWidth * 0.6
 
   const rectangles = sortItems.map(({ id, number }, idx) => {
+    const x = barWidth * idx * 2
+    const height = canvasContainerHeight * (number / sortItemsMax)
     return (
       <Rect
         key={id}
-        x={idx * 100}
+        x={x}
         y={50}
         ref={el => (sortItemRefs.current[id] = el)}
-        width={50}
-        height={number}
+        width={barWidth}
+        height={height}
         fill="#aaa"
       />
     )
   })
 
   const handleClick = () => {
+    if (isSorting) return
     setIsSorting(true)
-    const duration = 0.15
+    const duration = 0.1
     const array = [...sortItems]
     const n = array.length
     let count = 0
@@ -46,10 +55,10 @@ const BubbleSort = () => {
         const id2 = array[jj + 1].id
 
         // step1. move selected & change selectedBarColor
-        setTimeout(() => {
+        window.setTimeout(() => {
           new Konva.Tween({
             node: selectedItemRef.current,
-            x: jj * 100,
+            x: jj * barWidth * 2,
             duration,
           }).play()
 
@@ -68,10 +77,10 @@ const BubbleSort = () => {
 
         count++
         // step2. swap & change selectedBarColor
-        setTimeout(() => {
+        window.setTimeout(() => {
           new Konva.Tween({
             node: sortItemRefs.current[id1],
-            x: willSwap ? (jj + 1) * 100 : jj * 100,
+            x: willSwap ? (jj + 1) * barWidth * 2 : jj * barWidth * 2,
             duration,
             onFinish: () => {
               new Konva.Tween({
@@ -84,7 +93,7 @@ const BubbleSort = () => {
 
           new Konva.Tween({
             node: sortItemRefs.current[id2],
-            x: willSwap ? jj * 100 : (jj + 1) * 100,
+            x: willSwap ? jj * barWidth * 2 : (jj + 1) * barWidth * 2,
             duration,
             onFinish: () => {
               new Konva.Tween({
@@ -109,23 +118,66 @@ const BubbleSort = () => {
 
   return (
     <>
-      <Text variant="h4">Bubble Sort</Text>
-      <button onClick={handleClick}>SORT</button>
-      <div className="canvas-container">
-        <Stage width={width} height={window.innerHeight}>
-          <Layer>
-            <Group ref={el => (selectedItemRef.current = el)}>
-              {isSorting && (
-                <>
-                  <Rect x={10} y={10} width={30} height={10} fill="#f00" />
-                  <Rect x={110} y={10} width={30} height={10} fill="#f00" />
-                </>
-              )}
-            </Group>
-            {rectangles}
-          </Layer>
-        </Stage>
-      </div>
+      <Text className="mb-4" variant="h4">
+        Bubble Sort
+      </Text>
+
+      <section className="mb-4">
+        <Text variant="h6">Abstract</Text>
+        <Text variant="body2">
+          The Bubble Sort is probably the most simple of the sort algorithms.
+          <br />
+          <br />
+          If the given array has to be sorted in ascending order, then bubble sort will start by comparing the first
+          element of the array with the second element, if the first element is greater than the second element, it will
+          swap both the elements, and then move on to compare the second and the third element, and so on.
+        </Text>
+      </section>
+
+      <section className="mb-4">
+        <Text variant="h6">Complexity</Text>
+        <Text variant="body2">
+          The time complexity of Bubble Sort is O(n2). The main advantage of Bubble Sort is the simplicity of the
+          algorithm.
+          <br />
+          <br />
+          The space complexity for Bubble Sort is O(1), because only a single additional memory space is required
+        </Text>
+      </section>
+
+      <section>
+        <Text variant="h6">Visualization</Text>
+        <Button className="mb-4" variant="contained" color="primary" onClick={handleClick} disabled={isSorting}>
+          See Bubble Sort in action
+        </Button>
+        <div ref={canvasContainer} className="canvas-container">
+          <Stage width={bodyWidth} height={canvasContainerHeight}>
+            <Layer>
+              <Group ref={el => (selectedItemRef.current = el)}>
+                {isSorting && (
+                  <>
+                    <Rect
+                      x={selectedBarIndicatorWidth * 0.2}
+                      y={10}
+                      width={selectedBarIndicatorWidth}
+                      height={10}
+                      fill="#f00"
+                    />
+                    <Rect
+                      x={barWidth * 2 + selectedBarIndicatorWidth * 0.4}
+                      y={10}
+                      width={selectedBarIndicatorWidth}
+                      height={10}
+                      fill="#f00"
+                    />
+                  </>
+                )}
+              </Group>
+              {rectangles}
+            </Layer>
+          </Stage>
+        </div>
+      </section>
     </>
   )
 }
