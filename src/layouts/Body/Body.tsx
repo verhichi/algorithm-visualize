fe91fc8e48f6264b59e7e426a8e417705745c8f6
Daichi Nishida <verhichi@hotmail.com>
2020-05-10 22:57:35 +0900

    feat: edit Body and add bodyWidth Context

--- src/layouts/Body/Body.tsx
+++ src/layouts/Body/Body.tsx
@@ -1,10 +1,31 @@
-import React, { ReactNode } from 'react'
+import React, { useRef, useEffect, useState, ReactNode } from 'react'
+import { BodyWidthContext, getBodyWidth } from '.'
 import './styles.css'
 
 type Props = {
   children: ReactNode
 }
 
-const Body = ({ children }: Props) => <main className="body">{children}</main>
+const Body = ({ children }: Props) => {
+  const [width, setWidth] = useState(0)
+  const bodyRef = useRef(null)
+
+  useEffect(() => setWidth(getBodyWidth(bodyRef.current)), [bodyRef])
+
+  useEffect(() => {
+    const handleResize = () => setWidth(getBodyWidth(bodyRef.current))
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, []) // eslint-disable-line
+
+  return (
+    <BodyWidthContext.Provider value={width}>
+      <main ref={bodyRef} className="body">
+        {children}
+      </main>
+    </BodyWidthContext.Provider>
+  )
+}
 
 export default Body
